62da39a3a8154c1d1d5b6d29abe296bb17a08961
Aimeast <lixd3389@gmail.com>
2013-04-14 22:35:11 +0800

    Clean up

--- Bonobo.Git.Server/Bonobo.Git.Server/Global.asax.cs
+++ Bonobo.Git.Server/Bonobo.Git.Server/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.ServiceModel.Activation;
 using GitSharp;
 using Bonobo.Git.Server.Security;
 using Microsoft.Practices.Unity;
